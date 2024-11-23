4e95c013ac269c57d91c059f0b2323c41185e6fd
Christie Ballard <christie.ballard@riotinto.com>
2024-11-23 12:03:20 +1000

    halfway through class

--- src/main.jsx
+++ src/main.jsx
@@ -6,17 +6,13 @@ import HomePage from "./pages/HomePage.jsx";
 import ProjectPage from "./pages/ProjectPage.jsx";
 import NavBar from "./components/NavBar.jsx";
 
-
-
-
-
 const router = createBrowserRouter([
   {
     path: "/",
     element: <NavBar />,
     children: [
       { path: "/", element: <HomePage /> },
-      { path: "/project", element: <ProjectPage /> },
+      { path: "/project/:id", element: <ProjectPage /> },
     ],
   },
 ]);
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -1,14 +1,18 @@
-import { oneProject } from "../data";
+import { useParams } from "react-router-dom";
+import useProject from "../hooks/use-project";
 
 function ProjectPage() {
+    const { id } = useParams();
+    const { project, isLoading, error } = useProject(id);
+
     return (
     <div>
-        <h2>{oneProject.title}</h2>
-        <h3>Created at: {oneProject.date_created}</h3>
-        <h3>`Status: ${oneProject.is_open}`</h3>
+        <h2>{project.title}</h2>
+        <h3>Created at: {project.date_created}</h3>
+        <h3>{`Status: ${project.is_open}`}</h3>
         <h3>Pledges:</h3>
         <ul>
-            {oneProject.pledges.map((pledgesData, key) => {
+            {project.pledges.map((pledgeData, key) => {
                 return (
                     <li key={key}>
                         {pledgesData.amount} from {pledgesData.supporter}
