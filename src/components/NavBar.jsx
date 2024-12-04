199567fbc8a4bab6cb8aadde9b1631adb227d7bd
Christie Ballard <christie.ballard@riotinto.com>
2024-12-04 19:59:33 +1000

    css and nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,24 +2,29 @@ import { Link, Outlet } from "react-router-dom";
 import useAuth from "../hooks/use-auth";
 
 function NavBar() {
-    const {auth, setAuth} = useAuth();
+    const { auth, setAuth } = useAuth();
 
     const handleLogout = () => {
         window.localStorage.removeItem("token");
-        setAuth({ token : null });
+        setAuth({ token: null });
     };
 
     return (
         <div>
             <nav>
+                {/* Link to Home */}
                 <Link to="/">Home</Link>
+
+                {/* Conditional Rendering for Login/Logout */}
                 {auth.token ? (
                     <Link to="/" onClick={handleLogout}>
                         Log Out
                     </Link>
-                    ) : (
+                ) : (
                     <Link to="/login">Login</Link>
                 )}
+                {/* Link to Create Project */}
+                <Link to="/create-project">Create Project</Link>
             </nav>
             <Outlet />
         </div>
